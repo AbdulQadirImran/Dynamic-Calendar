a8568479e99f68d91264593667a084d2987fc89d
Abdul Qadir Imran <abdulqadirimran01@gmail.com>
2024-05-14 21:02:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const daysTag = document.querySelector(".days"),
 currentDate = document.querySelector(".current-date"),
 prevNextIcon = document.querySelectorAll(".icons span");
-
 // getting new date, current year and month
 let date = new Date(),
 currYear = date.getFullYear(),
